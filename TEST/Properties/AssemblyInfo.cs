3d4ad7ddca2760d5d9dde1e1adb7844a783b20bd
SAMI KHALIFAH <shkhalifah17@cit.just.edu.jo>
2022-06-04 22:23:24 +0300

    Add project files.

---
+++ TEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TEST")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14e84578-d35e-417c-9526-05c75d6ce788")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TEST/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Docker.DotNet.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace TEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            By bott = By.Name("LOGIN");
+            By UERNAM = By.Id("Input_Email");
+            By pass = By.Id("Input_Password");
+            By log = By.Name("login");
+           //By tour = By.Id("navbarDropdown");
+           // By maketour = By.XPath(".//*[@id='sami1']");
+
+
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl("https://localhost:44356/");
+
+            SelectElement oSelect = new SelectElement(webDriver.FindElement(By.Id("navbarDropdown")));
+
+            oSelect.SelectByValue("Create Tour");
+
+
+
+
+            webDriver.FindElement(bott).Click();
+            webDriver.FindElement(UERNAM).SendKeys("ss@f");
+            webDriver.FindElement(pass).SendKeys("Sjl/21l#");
+            //Thread.Sleep(5000);
+            webDriver.FindElement(log).Click();
+            //webDriver.FindElement(tour).Click();
+            //webDriver.FindElement(maketour).Click();
+
+
+//webDriver.Quit();
+
+
+        }
+    }
+}
